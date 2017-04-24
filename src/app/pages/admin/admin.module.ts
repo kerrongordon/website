7445dce45e2071542e48f4a0b821b840e70ab05e
kerrongordon <kgpsounds.com@gmail.com>
2017-04-23 22:17:00 -0400

    working on admin page

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { HeadModule } from '../../components/head/head.module';
+import { CardModule } from '../../components/card/card.module';
 import { FooterModule } from '../../components/footer/footer.module';
 
 @NgModule({
   imports: [
     CommonModule,
     HeadModule,
+    CardModule,
     FooterModule,
     RouterModule.forChild([
       { path: '', component: AdminComponent }
--- src/app/pages/admin/admin/admin.component.ts
+++ src/app/pages/admin/admin/admin.component.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { DatabaseService } from '../../../services/database.service';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'kg-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.sass'],
-  providers: [DatabaseService]
+  providers: [DatabaseService, AuthService]
 })
 export class AdminComponent implements OnInit {
 
   infor: any;
+  public auth: any;
+  public portfolios: any;
 
-  constructor(public ds: DatabaseService) { }
+  constructor(private db: DatabaseService, private user: AuthService) { }
 
   ngOnInit() {
     // this.ds.homePageInfor().subscribe(data => this.infor = data);
+    this.userInfor();
+    this.getPortfolios();
+  }
+
+  public userInfor() {
+  	return this.user.isAuth().subscribe(data => this.auth = data.google);
+  }
+
+  public getPortfolios() {
+  	this.db.getPortfolios().subscribe(data => this.portfolios = data);
   }
 
 }
