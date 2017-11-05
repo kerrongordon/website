64dc4da04b5afea3d953c34309d000a6306c5b5a
kerrongordon <kgpsounds.com@gmail.com>
2017-11-05 18:55:35 -0400

    added hero component

---
+++ src/app/components/hero/hero/hero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kgp-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.sass']
+})
+export class HeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
