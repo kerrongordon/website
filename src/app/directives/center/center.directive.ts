f797a00aee5474f043caafec15f6e95714a19ae9
kerrongordon <kgpsounds.com@gmail.com>
2017-11-06 15:34:08 -0400

    added center

---
+++ src/app/directives/center/center.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[Center]'
+})
+export class CenterDirective {
+
+  constructor(
+    private _ElementRef: ElementRef
+  ) { 
+    _ElementRef.nativeElement.style.display = 'flex'
+    _ElementRef.nativeElement.style.alignItems = 'center'
+    _ElementRef.nativeElement.style.justifyContent = 'center'
+   }
+
+}
