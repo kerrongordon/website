568c8ef26e85b0cf6ba85010b43448d11c90e046
kerrongordon <kgpsounds.com@gmail.com>
2017-11-06 15:35:17 -0400

    update home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,12 +3,25 @@ import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
 import { HomeComponent } from './home/home.component'
 
+import { PrimaryModule } from '../../directives/primary/primary.module'
+import { CenterModule } from '../../directives/center/center.module'
+import { ContainerModule } from '../../directives/container/container.module'
+import { CinzelModule } from '../../directives/cinzel/cinzel.module'
+
 import { HeroModule } from '../../components/hero/hero.module'
+import { AvatarModule } from '../../components/avatar/avatar.module'
+import { DescriptionModule } from '../../components/description/description.module'
 
 @NgModule({
   imports: [
     CommonModule,
+    PrimaryModule,
+    CenterModule,
+    ContainerModule,
+    CinzelModule,
     HeroModule,
+    AvatarModule,
+    DescriptionModule,
     RouterModule.forChild([
       { path: '', component: HomeComponent }
     ])
