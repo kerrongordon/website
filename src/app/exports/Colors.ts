b302a997ae96f6760bfbaac75e201ae5f35e3293
kerrongordon <kgpsounds.com@gmail.com>
2017-11-06 15:34:50 -0400

    added colors

---
+++ src/app/exports/Colors.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+    'primary': '#2798ee',
+    'secondary': '#7cb342',
+    'danger': '#f44336',
+    'background': '#ecf0f1',
+    'fontPrimary': '#6b7c93',
+    'fontSecondary': '#999999',
+    'light': '#ffffff'
+}
