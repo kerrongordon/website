a113f10b060dbd07a4075ac740e943284df2f0fb
kerrongordon <kgpsounds.com@gmail.com>
2017-11-06 15:32:44 -0400

    added description

---
+++ src/app/components/description/description/description.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'kgp-description',
+  templateUrl: './description.component.html',
+  styleUrls: ['./description.component.sass']
+})
+export class DescriptionComponent {
+  @Input() content: string
+}
