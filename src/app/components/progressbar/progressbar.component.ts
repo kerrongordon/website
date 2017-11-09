daaf98141dd9498bda8542b0dbd896641ab48591
kerrongordon <kgpsounds.com@gmail.com>
2017-11-09 16:32:36 -0400

    added ProgressbarComponent

---
+++ src/app/components/progressbar/progressbar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'kgp-progressbar',
+  templateUrl: './progressbar.component.html',
+  styleUrls: ['./progressbar.component.sass'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ProgressbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
