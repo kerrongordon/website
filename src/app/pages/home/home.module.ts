af0e9c9cd3ebc855fb3506bc63d2713e760f6a4a
kerrongordon <kgpsounds.com@gmail.com>
2017-11-09 16:45:20 -0400

    update home component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home.component'
 
 import { PrimaryModule } from '../../directives/primary/primary.module'
 import { CenterModule } from '../../directives/center/center.module'
