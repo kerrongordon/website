76ab03781f0570b894324062730a1d0efbc7ecf1
kerrongordon <kgpsounds.com@gmail.com>
2017-11-12 10:37:54 -0400

    added truncate

--- src/app/components/card-content/card-content.module.ts
+++ src/app/components/card-content/card-content.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CardContentComponent } from './card-content.component';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { CardContentComponent } from './card-content.component'
+import { TruncatePipe } from '../../pipe/truncate/truncate.pipe'
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [CardContentComponent],
+  declarations: [CardContentComponent, TruncatePipe],
   exports: [CardContentComponent]
 })
 export class CardContentModule { }
