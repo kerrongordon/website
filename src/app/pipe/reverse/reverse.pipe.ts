bc727fe044902ca1dddcd965095929d3f1f96760
kerrongordon <kgpsounds.com@gmail.com>
2017-11-20 20:56:31 -0400

    added reverse pipe

---
+++ src/app/pipe/reverse/reverse.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core'
+
+@Pipe({
+  name: 'reverse',
+  pure: true
+})
+export class ReversePipe implements PipeTransform {
+
+  transform(value: any): any {
+    if (value === null) return
+    return value.reverse()
+  }
+
+}
