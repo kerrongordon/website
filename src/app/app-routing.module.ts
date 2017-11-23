5d8e5c60a6b81e173531d638e715c71fc53b4988
kerrongordon <kgpsounds.com@gmail.com>
2017-11-23 11:47:28 -0400

    change router config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,9 @@ import { Routes, RouterModule } from '@angular/router'
 
 const routes: Routes = [
   { path: '', loadChildren: './pages/home/home.module#HomeModule' },
-  { path: 'dashboard',
-    children: [
-      { path: '', loadChildren: './pages/dashboard/dashboard.module#DashboardModule' },
-      { path: 'email', loadChildren: './pages/email/email.module#EmailModule' }
-    ]
-  }
+  { path: 'dashboard', loadChildren: './pages/dashboard/dashboard.module#DashboardModule' },
+  { path: 'email', loadChildren: './pages/email/email.module#EmailModule' },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ]
 
 @NgModule({
