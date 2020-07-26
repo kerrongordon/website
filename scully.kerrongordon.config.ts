52e786efd96649e83f5d1e7ad961a78f698b4148
kerrongordon <kgpsounds.com@gmail.com>
2020-07-25 23:11:16 -0400

    remove unsed code

--- scully.kerrongordon.config.ts
+++ scully.kerrongordon.config.ts
@@ -1,6 +1,5 @@
 import { ScullyConfig } from '@scullyio/scully';
 import { MinifyHtml } from 'scully-plugin-minify-html';
-// const lazyImages = require('@notiz/scully-plugin-lazy-images');
 // import { DisableAngular } from 'scully-plugin-disable-angular';
 
 const postRenderers = [MinifyHtml];
